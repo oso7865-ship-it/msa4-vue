756b1c76d7947b00a126aa8ac457d103d098e2a2
oso7865-ship-it <oso7865@gmail.com>
2026-04-16 17:06:11 +0900

    260416 vue init ~ dataBinding

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// js 코드 작성 부분
+import DataBinding from './components/DataBinding.vue';
+</script>
+
+<template>
+<!-- HTML 작성 부분 -->
+  <DataBinding />
+</template>
+
+<style>
+/* CSS 작성부분 */
+</style>
